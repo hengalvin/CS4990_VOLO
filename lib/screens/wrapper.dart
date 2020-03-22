a0d5770a3803a3890601e2c81486a516ec9ba2a5
hengalvin <alvin.hadiheng@gmail.com>
2020-03-21 18:00:00 -0700

    firebase auth anon

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:volo/screens/authenticate/authenticate.dart';
+import 'package:volo/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    //return home or authenticate
+    return Authenticate();
+
+  }
+}
